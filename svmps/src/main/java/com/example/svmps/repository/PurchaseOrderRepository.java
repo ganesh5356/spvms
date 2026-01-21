806e196d70fd317d9afec6d01a19d4fc4dba98a3
Juhee Saxena <juheesaxena57@gmail.com>
2026-01-21 23:06:33 +0530

    spvms dupliacte deleted

--- svmps/src/main/java/com/example/svmps/repository/PurchaseOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.svmps.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.svmps.entity.PurchaseOrder;
-
-public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
-}
